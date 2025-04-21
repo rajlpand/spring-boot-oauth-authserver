86c340e03db397c818fff08250ef359fea28f864
Rajnish Lal <rajnish.lal@panduit.com>
2025-04-21 11:01:00 -0700

    Create Spring Boot Project

---
+++ src/main/java/com/panduit/sbauth/springbootoauthauthserver/SpringBootOauthAuthserverApplication.java
@@ -0,0 +1,13 @@
+package com.panduit.sbauth.springbootoauthauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthAuthserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauthAuthserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/panduit/sbauth/springbootoauthauthserver/SpringBootOauthAuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panduit.sbauth.springbootoauthauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauthAuthserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
