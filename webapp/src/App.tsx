b6551b1890167215451a6c82cfdd725cd32f1f32
Rajnish Lal <rajnish.lal@panduit.com>
2025-04-23 17:21:59 -0700

    OAuth setup

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {JSX, useContext} from 'react'
+import {
+    AuthContext,
+    AuthProvider,
+    type IAuthContext,
+    type TAuthConfig,
+    TRefreshTokenExpiredEvent
+} from 'react-oauth2-code-pkce'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const authConfig: TAuthConfig = {
+    clientId: 'oidc-client',
+    authorizationEndpoint: 'http://localhost:9000/oauth2/authorize',
+    tokenEndpoint: 'http://localhost:9000/oauth2/token',
+    redirectUri: 'http://localhost:8080/', // Ensure this matches your app's URL
+    scope: 'openid profile', // Adjust scopes as needed
+    onRefreshTokenExpire: (event: TRefreshTokenExpiredEvent) => window.confirm('Session expired. Refresh page to continue using the site?') && event.login(),
 }
 
-export default App
+function LoginInfo(): JSX.Element {
+    const { tokenData, token, logIn, logOut, error }: IAuthContext = useContext(AuthContext)
+
+    if (error) {
+        return (
+            <>
+                <div style={{ color: 'red' }}>An error occurred during authentication: {error}</div>
+                <button onClick={() => logOut()}>Log out</button>
+            </>
+        )
+    }
+
+    return (
+        <>
+            {token ? (
+                <>
+                    <div>
+                        <h4>Access Token (JWT)</h4>
+                        <pre
+                            style={{
+                                width: '400px',
+                                margin: '10px',
+                                padding: '5px',
+                                border: 'black 2px solid',
+                                wordBreak: 'break-all',
+                                whiteSpace: 'break-spaces',
+                            }}
+                        >
+              {token}
+            </pre>
+                    </div>
+                    <div>
+                        <h4>Login Information from Access Token (Base64 decoded JWT)</h4>
+                        <pre
+                            style={{
+                                width: '400px',
+                                margin: '10px',
+                                padding: '5px',
+                                border: 'black 2px solid',
+                                wordBreak: 'break-all',
+                                whiteSpace: 'break-spaces',
+                            }}
+                        >
+              {JSON.stringify(tokenData, null, 2)}
+            </pre>
+                    </div>
+                    <button onClick={() => logOut()}>Log out</button>
+                </>
+            ) : (
+                <>
+                    <div>You are not logged in.</div>
+                    <button onClick={() => logIn()}>Log in</button>
+                </>
+            )}
+        </>
+    )
+}
+
+function App() {
+    return (
+        <div>
+            <div>
+                <h1>Demo using the 'react-oauth2-code-pkce' package</h1>
+            </div>
+            <AuthProvider authConfig={authConfig}>
+                {/* @ts-ignore*/}
+                <LoginInfo />
+            </AuthProvider>
+        </div>
+    );
+}
+
+export default App;
